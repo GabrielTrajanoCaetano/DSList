1a25bba3ae00ed2eeb474e29e320da9fd0f9ef52
Gabriel Trajano Caetano <gabriel.caetanotrajano@hotmail.com>
2025-08-19 16:03:50 -0300

    First commit

---
+++ src/test/java/com/gabrielbackend/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielbackend.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
