a36ebd472d029af9c59883fe6f44831e6c7f51a5
Gabriel Trajano Caetano <gabriel.caetanotrajano@hotmail.com>
2025-08-22 16:20:03 -0300

    Homolog ok

--- src/main/java/com/gabrielbackend/dslist/dto/GameMinDto.java
+++ src/main/java/com/gabrielbackend/dslist/dto/GameMinDto.java
@@ -30,7 +30,7 @@ public class GameMinDto {
     public GameMinDto (GameMinProjection entity){
         id = entity.getId();
         title = entity.getTitle();
-        year = entity.getYear();
+        year = entity.getGameYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
     }
@@ -43,7 +43,7 @@ public class GameMinDto {
         return title;
     }
 
-    public Integer getYear() {
+    public Integer getGameYear() {
         return year;
     }
 
