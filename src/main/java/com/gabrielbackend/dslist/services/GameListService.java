5e983bf1480d7960df8af914b901955317a7b49c
Gabriel Trajano Caetano <gabriel.caetanotrajano@hotmail.com>
2025-08-22 23:37:15 -0300

    Working at move element of position

--- src/main/java/com/gabrielbackend/dslist/services/GameListService.java
+++ src/main/java/com/gabrielbackend/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.gabrielbackend.dslist.services;
 
 import com.gabrielbackend.dslist.dto.GameListDto;
 import com.gabrielbackend.dslist.entities.GameList;
+import com.gabrielbackend.dslist.projections.GameMinProjection;
 import com.gabrielbackend.dslist.repositories.GameListRepository;
+import com.gabrielbackend.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +17,23 @@ public class GameListService {
     @Autowired
     public GameListRepository gameListRepository;
 
+    @Autowired
+    public GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDto> findAllList(){
         List<GameList> lists = gameListRepository.findAll();
         return lists.stream().map(x-> new GameListDto(x)).toList();
+
+    }
+
+    public void move(Long listId, int sourceIndex, int destinationIndex){
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection game = list.remove(sourceIndex);
+        list.add(destinationIndex, game);
+
 
     }
 }
